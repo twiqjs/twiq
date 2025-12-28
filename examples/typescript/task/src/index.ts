4dd1594cafd70a63940c014fe2dd134fca18466b
high-u <zen.crazyd@gmail.com>
2025-12-29 08:56:04 +0900

    fix

--- examples/typescript/task/src/index.ts
+++ examples/typescript/task/src/index.ts
@@ -21,7 +21,6 @@ const getTasks = () => {
   });
 };
 
-// Component creators to reuse props logic
 const createTaskList = () => {
   const props: TaskListProps = {
     tasks: store.tasks,
@@ -47,7 +46,6 @@ const createControls = () => {
   });
 };
 
-// Update functions
 const renderTaskList = () => {
   mount('list-wrapper', createTaskList());
 };
@@ -58,7 +56,6 @@ const renderControls = () => {
 
 const App = () => div({ id: 'frame', class: "bg-main app-py col" },
   div({ class: 'app-px' }, Header()),
-  // Nested declarative structure: Components are rendered immediately
   div({ id: 'controls-container', class: 'app-px' }, createControls()),
   div({ id: 'list-wrapper', class: 'app-px grow' }, createTaskList()),
   div({ class: 'app-px' }, Footer()),
@@ -66,9 +63,7 @@ const App = () => div({ id: 'frame', class: "bg-main app-py col" },
 
 mount('app', App());
 
-// Initial load
 getTasks().then((tasks) => {
   store.setTasks(tasks);
   renderTaskList();
-  // Controls doesn't need update on data fetch, but consistent with previous logic
 });
